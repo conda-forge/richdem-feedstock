f67b759d7c79399313b43068a3277da31d266be2
Howard Butler <hobu.inc@gmail.com>
2022-02-11 08:25:38 -0600

    apply python-pdal-feedstock patterns for osx-arm64 xcompile

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,27 @@
-export CXXFLAGS="-std=c++11"
-# export CXXFLAGS="-std=c++17"
-export MACOSX_DEPLOYMENT_TARGET="10.15"
+#!/bin/bash
+
+
+set -ex
+
+# strip std settings from conda
+CXXFLAGS="${CXXFLAGS/-std=c++14/}"
+CXXFLAGS="${CXXFLAGS/-std=c++11/}"
+export CXXFLAGS
+
+if [ "$target_platform" = "osx-arm64" ]; then
+  export CMAKE_OSX_ARCHITECTURES="arm64"
+  rm -rf $BUILD_PREFIX/bin/x86_64-*
+fi
+
+
 
 mkdir _build && cd _build
 cmake .. \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
   -DCMAKE_BUILD_TYPE=Release \
-  -DUSE_GDAL=ON \
-  -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 \
-  -DCMAKE_CXX_COMPILER=$CXX
+  -DUSE_GDAL=ON
 
 make all -j$CPU_COUNT
 make install
 
-cd ../wrappers/pyrichdem && python -m pip install . --no-deps -vv
+cd ../wrappers/pyrichdem && ${PYTHON} -m pip install . --no-deps -vv
